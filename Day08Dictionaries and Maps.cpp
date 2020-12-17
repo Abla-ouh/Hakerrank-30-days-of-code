8818fc1fd2c84d52b205ee4ac1af192d3f155a92
Abla-ouh <57642906+Abla-ouh@users.noreply.github.com>
2020-12-17 23:48:10 +0100

    Update Day08Dictionaries and Maps.cpp

--- Day08Dictionaries and Maps.cpp
+++ Day08Dictionaries and Maps.cpp
@@ -9,35 +9,23 @@ using namespace std;
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
-    int t; // test cases
-cin >> t;
-
-string phonebook [t][2];
-string names [t];
-
-
-int i,j,k,f; //counter
-
-
-for (i = 0; i <t; i++){ //creating the phonebook
-
-        cin >> phonebook [i][0] >>  phonebook [i][1];
-        }
-
-for (k = 0;k<t;k++){
-    cin >> names[k];
-    int x =0; // for testing
-    for (f=0; f<t;f++){
-    if(names[k] == phonebook[f][0]){
-        cout << phonebook[f][0] << "=" << phonebook[f][1] <<endl;
-        x=1;
-    }
-    }
-    if(x!=1){
-        cout << "Not found\n";
+int n;
+string name;
+long num;
+cin >> n;
+cin.ignore();
+map <string, long> pBook;
+for (int i = 0; i < n; i++) {
+    cin >> name;
+    cin >> num;
+    pBook[name] = num;
+}
+while(cin >> name) {
+    if (pBook.find(name) != pBook.end()) {
+        cout << name << "=" << pBook.find(name)->second << endl;
+    } else {
+        cout << "Not found" << endl;
     }
 }
-    
-     
     return 0;
 }
